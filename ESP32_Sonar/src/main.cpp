f040fc0ed7cb5c53b3056a0cd8966d4037bbf0ae
Klugame <ligueketoniejudo@gmail.com>
2025-05-05 12:02:49 +0200

    mise en place bt

--- ESP32_Sonar/src/main.cpp
+++ ESP32_Sonar/src/main.cpp
@@ -1,27 +1,18 @@
-#include <Arduino.h>
+#include "BluetoothSerial.h"
 
-// Déclaration des pin
-const int trig = 25;
-const int echo = 33;
+BluetoothSerial SerialBT;
 
 void setup() {
-    Serial.begin(9600);  // Initialisation du moniteur série
-    pinMode(trig, OUTPUT);  // Définir la broche trig comme sortie
-    pinMode(echo, INPUT);  // Définir la broche echo comme entrée
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_BT", true);  // true = SPP esclave
+  SerialBT.setPin("1234");           // Important !
+  Serial.println("Bluetooth actif avec nom ESP32_BT et PIN 1234");
 }
 
 void loop() {
-    digitalWrite(trig, LOW);  // Assurez-vous que le trig est à LOW
-    delayMicroseconds(2);  // Attendre 2 microsecondes
-    digitalWrite(trig, HIGH);  // Envoyer un signal HIGH sur le trig
-    delayMicroseconds(10);  // Attendre 10 microsecondes
-    digitalWrite(trig, LOW);  // Passer à LOW
-    int duration = pulseIn(echo, HIGH);  // Lire la durée du signal HIGH sur le echo
-    float distance = (duration * 0.03432) / 2;  // Calculer la distance en cm
-    Serial.print("Distance: ");  // Afficher la distance
-    Serial.print(distance);  // Afficher la distance calculée
-    Serial.println(" cm");  // Afficher l'unité de mesure
-
-
-    delay(500);  // Pause de 500ms avant la prochaine lecture
+  if (SerialBT.available()) {
+    char c = SerialBT.read();
+    Serial.write(c);
+    SerialBT.write(c);
+  }
 }
