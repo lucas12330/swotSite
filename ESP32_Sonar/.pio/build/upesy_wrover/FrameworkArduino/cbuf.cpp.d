b9dc2aa0d6bdb3da0da30cfa06828a48d6f7129c
Klugame <ligueketoniejudo@gmail.com>
2025-05-09 13:52:01 +0200

    ESP32 Arduino

---
+++ ESP32_Sonar/.pio/build/upesy_wrover/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/upesy_wrover/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/Lucas\ Bezanilla/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/Lucas\ Bezanilla/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
