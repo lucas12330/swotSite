9d2964d6c9b1fcfbfabab7ec3b2ae1bbc4d2cd8d
Klugame <ligueketoniejudo@gmail.com>
2025-05-12 11:21:39 +0200

    Fichier temporaire et ajout de MQTT php

---
+++ ESP32_POST/.pio/build/upesy_wrover/FrameworkArduino/USB.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/upesy_wrover/FrameworkArduino/USB.cpp.o: \
+ C:/Users/Lucas\ Bezanilla/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
+ C:/Users/Lucas\ Bezanilla/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/Lucas\ Bezanilla/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
---
+++ ESP32_POST/.pio/build/upesy_wrover/FrameworkArduino/esp32-hal-tinyusb.c.d
@@ -0,0 +1,3 @@
+.pio/build/upesy_wrover/FrameworkArduino/esp32-hal-tinyusb.c.o: \
+ C:/Users/Lucas\ Bezanilla/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-tinyusb.c \
+ C:/Users/Lucas\ Bezanilla/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
